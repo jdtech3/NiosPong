e8328ee463b7d5bbec38d0329f8148c389c4a599
Joe Dai <dm.joedai@gmail.com>
2024-03-06 14:42:40 -0500

    Boilerplate

---
+++ include/constants.h
@@ -0,0 +1,11 @@
+#ifndef __CONSTANTS_H
+#define __CONSTANTS_H
+
+#define EDGE_CAP_OFFSET         3
+
+#define AUDIO_FIFOSPACE_OFFSET  1
+#define AUDIO_LEFT_OFFSET       2
+#define AUDIO_RIGHT_OFFSET      3
+#define AUDIO_SAMPLES_PER_SEC   8000
+
+#endif  // __CONSTANTS_Hss
---
+++ src/main.c
@@ -0,0 +1,9 @@
+#include <stdlib.h>
+#include <stdbool.h>
+
+#include "address_map_nios2.h"
+#include "constants.h"
+
+int main() {
+    return 0;
+}
